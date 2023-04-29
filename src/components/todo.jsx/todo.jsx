cef249cb06cf10ee056a96329cbebf9b882d7c7c
lucass2k <lucasnahuel2000silva@gmail.com>
2023-04-29 00:19:24 -0300

    first commit

---
+++ src/components/todo.jsx/todo.jsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+export default function Task({ item, onUpdate, onComplete, onDelete }) {
+  const [isEdit, setIsEdit] = useState(false);
+  const [value, setValue] = useState(item.title ?? "");
+
+  function handleChange(e) {
+    setValue(e.target.value);
+  }
+
+  function handleUpdate() {
+    onUpdate(item.id, value);
+    setIsEdit(false);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    onUpdate(item.id, value);
+    setIsEdit(false);
+  }
+
+  function handleCheckboxChange(e) {
+    onComplete(item.id, e.target.checked);
+  }
+
+  return (
+    <div className="task">
+      {isEdit ? (
+        <form
+          onSubmit={handleSubmit}
+          className="taskUpdateForm"
+          placeholder="Ingrese una tarea"
+        >
+          <input
+            className="taskInput"
+            type="text"
+            value={value}
+            onChange={handleChange}
+          />
+          <button className="button" onClick={handleUpdate}>
+            Guardar
+          </button>
+        </form>
+      ) : (
+        <div className="taskInfo">
+          <span className="taskTitle">{item.title}</span>
+          <button className="button" onClick={() => setIsEdit(true)}>
+            Editar
+          </button>
+          <button className="buttonDelete" onClick={() => onDelete(item.id)}>
+            Eliminar
+          </button>
+        </div>
+      )}
+    </div>
+  );
+}
---
+++ src/components/todo.jsx/todoapp.jsx
@@ -0,0 +1,73 @@
+import { useState } from "react";
+import Todo from "./todo";
+export default function TodoApp() {
+  const [title, setTitle] = useState("");
+  const [tasks, setTasks] = useState([]);
+  function handleInputChange(e) {
+    setTitle(e.target.value);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const newTask = {
+      id: Date.now(),
+      title: title,
+      completed: false,
+    };
+
+    const oldTasks = [...tasks];
+    oldTasks.unshift(newTask);
+
+    setTasks(oldTasks);
+    setTitle("");
+  }
+  function handleDelete(id) {
+    const tempTasks = tasks.filter((item) => item.id !== id);
+
+    setTasks([...tempTasks]);
+  }
+
+  function handleUpdate(id, value) {
+    const temp = [...tasks];
+    const item = temp.find((item) => item.id === id);
+    item.title = value;
+    setTasks([...temp]);
+  }
+  function handleDeleteAll() {
+    setTasks([]);
+  }
+
+  function handleCheckboxChange(id, status) {
+    const temp = [...tasks];
+    const item = temp.find((item) => item.id === id);
+    item.completed = status;
+    setTasks([...temp]);
+  }
+
+  return (
+    <div className="taskContainer">
+      <form onSubmit={handleSubmit} className="taskCreateForm">
+        <input
+          onChange={handleInputChange}
+          value={title}
+          className="taskInput"
+        />
+        <input value="Crear tarea" type={"submit"} className="buttonCreate" />
+      </form>
+      <button className="buttonDelete" onClick={() => handleDeleteAll()}>
+        Eliminar todas
+      </button>
+      <div className="tasksContainer">
+        {tasks.map((item) => (
+          <Todo
+            key={item.id}
+            item={item}
+            onUpdate={handleUpdate}
+            onDelete={handleDelete}
+            onComplete={handleCheckboxChange}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
